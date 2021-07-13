5d81353070c7fba8c53c4eaa2cd45e56efeef2e5
Jeff Kletsky <git-commits@allycomm.com>
2021-07-12 18:52:27 -0700

    Update examples/find_first_and_load.py to show tracking "ready"
    With "ready" available from the connectivity query, it is now possible
    to check if the device is already connected, connect if not, and then
    wait until the device is ready for further commands.
    
    Implementation in this example is done with the paho and request modules
    and "classic" threading is used.

--- examples/find_first_and_load.py
+++ examples/find_first_and_load.py
@@ -5,16 +5,20 @@ License for this software, part of the pyDE1 package, is granted under
 GNU General Public License v3.0 only
 SPDX-License-Identifier: GPL-3.0-only
 
-Perhaps a rimmed-down version of explorations/test_find_first_and_load.py
+Since the requests module isn't asyncio aware and the command server
+is sequential on how it handles requests, accept blocking on the main
+thread and run the MQTT client in its own thread.
 """
+from pyDE1.event_manager.events import ConnectivityState
 
 DISCONNECT_AT_END = True
 
-import asyncio
+
 import json
 import logging
 import os
 import queue
+import threading
 import time
 
 from socket import gethostname
@@ -27,16 +31,16 @@ import requests
 
 from pyDE1.dispatcher.resource import Resource, DE1ModeEnum, ConnectivityEnum
 
-logger = logging.getLogger('main')
-logger_scanner = logging.getLogger('Scanner')
-logger_connect = logging.getLogger('Connect')
 
-logging.getLogger('asyncio').setLevel(logging.INFO)
 
 format_string = "%(asctime)s %(levelname)s %(name)s: %(message)s"
 logging.basicConfig(level=logging.DEBUG,
                     format=format_string,
                     )
+
+logger = logging.getLogger('main')
+logger_scanner = logging.getLogger('Scanner')
+logger_connect = logging.getLogger('Connect')
 
 MQTT_TOPIC_ROOT = 'pyDE1'
 
@@ -56,88 +60,130 @@ SERVER_HOST = 'localhost'
 SERVER_PORT = 1234
 SERVER_ROOT = '/'
 
+DEBUG_MQTT = False
+
+
+# Utilities for inbound API requests
 
 def path_of(res: Resource):
     return f"http://{SERVER_HOST}:{SERVER_PORT}" \
            f"{os.path.join(SERVER_ROOT, res.value)}"
 
 
-async def run():
+def show_result(r: requests.Response):
+    body = r.request.body
+    if isinstance(body, (bytes, bytearray)):
+        body = body.decode('utf-8')
+    logger.debug(f"{r.status_code} {r.request.method} {r.request.url}\n"
+          f"{body}\n{r.text}")
 
-    def show_result(r: requests.Response):
-        body = r.request.body
-        if isinstance(body, (bytes, bytearray)):
-            body = body.decode('utf-8')
-        logger.debug(f"{r.status_code} {r.request.method} {r.request.url}\n"
-              f"{body}\n{r.text}")
 
-    have_de1 = asyncio.Event()
-    have_scale = asyncio.Event()
+# Specific inbound API requests
 
-    def is_connected(res: Resource):
+def find_first(res: Resource):
+    t0 = time.time()
+    r = requests.patch(path_of(res),
+                       json = {"first_if_found": True})
+    t1 = time.time()
+    show_result(r)
+    logger.debug(f"{res} elapsed time: {(t1 - t0):.3f} seconds")
+
+def connect(res: Resource, id: Optional[str]):
+    t0 = time.time()
+    r = requests.patch(path_of(res),
+                       json = {"id": id})
+    t1 = time.time()
+    show_result(r)
+    logger.debug(f"{res} elapsed time: {(t1 - t0):.3f} seconds")
+
+def upload_profile(filename: str):
+    logger.debug(f"cwd: {os.path.abspath(os.curdir)}")
+    t0 = time.time()
+    with open(filename, 'rb') as profile:
+        logger.debug(f"opened: {filename}")
+        r = requests.put(path_of(Resource.DE1_PROFILE),
+                         data = profile)
+    t1 = time.time()
+    show_result(r)
+    logger.debug(f"Upload profile elapsed time: {(t1 - t0):.3f} seconds")
+
+
+def set_saw(mass: float):
+    r = requests.patch(path_of(Resource.DE1_CONTROL_ESPRESSO),
+                       json={"stop_at_time": None,
+                             "stop_at_volume": None,
+                             "stop_at_weight": 50})
+    show_result(r)
+
+
+def run():
+
+    have_de1 = threading.Event()
+    have_scale = threading.Event()
+
+    ready_de1 = threading.Event()
+    ready_scale = threading.Event()
+
+    run_start = time.time()
+
+    def check_connectivity(res: Resource,
+                           have: threading.Event,
+                           ready: threading.Event):
+        """
+        If already connected, set the "have" event
+        If also ready, set the "ready" event
+        """
         r = requests.get(path_of(res))
         show_result(r)
-        retval = None
         try:
-            retval = r.json()['mode'] == ConnectivityEnum.CONNECTED.value
+            if r.json()['mode'] == ConnectivityEnum.READY.value:
+                have.set()
+                ready.set()
+            elif r.json()['mode'] == ConnectivityEnum.CONNECTED.value:
+                have.set()
+                logger.warning("{res} is connected, but not ready")
         finally:
-            return retval
+            return
 
-    if is_connected(Resource.DE1_CONNECTIVITY):
-        have_de1.set()
 
-    if is_connected(Resource.SCALE_CONNECTIVITY):
-        have_scale.set()
-
-    def find_first(res: Resource):
-        t0 = time.time()
-        r = requests.patch(path_of(res),
-                           json = {"first_if_found": True})
-        t1 = time.time()
-        show_result(r)
-        logger.debug(f"{res} elapsed time: {(t1 - t0):.3f} seconds")
-
-    def connect(res: Resource, id: Optional[str]):
-        t0 = time.time()
-        r = requests.patch(path_of(res),
-                           json = {"id": id})
-        t1 = time.time()
-        show_result(r)
-        logger.debug(f"{res} elapsed time: {(t1 - t0):.3f} seconds")
-
-    def upload_profile(filename: str):
-        logger.debug(f"cwd: {os.path.abspath(os.curdir)}")
-        with open(filename, 'rb') as profile:
-            logger.debug(f"opened: {filename}")
-            r = requests.put(path_of(Resource.DE1_PROFILE),
-                             data = profile)
-        show_result(r)
-
-    def set_saw(mass: float):
-        r = requests.patch(path_of(Resource.DE1_CONTROL_ESPRESSO),
-                           json={"stop_at_time": None,
-                                 "stop_at_volume": None,
-                                 "stop_at_weight": 50})
-        show_result(r)
-
-    def on_connect_callback(client, userdata, flags, reasonCode, properties):
-        logger.debug(f"CB: Connect: flags: {flags}, reasonCode: {reasonCode}, "
-              f"properties {properties}")
+    # Set up the MQTT client
 
     topic_scanner = f"{MQTT_TOPIC_ROOT}/ScannerNotification"
     topic_connect = f"{MQTT_TOPIC_ROOT}/ConnectivityChange"
 
-    def scan_message_cb(client: mqtt.Client, userdata, message: MQTTMessage):
+    def on_connect_callback(client, userdata, flags, reasonCode, properties):
+        logger.debug(f"MQTT Connect: flags: {flags}, reasonCode: {reasonCode}, "
+              f"properties {properties}")
+        client.subscribe(topic_scanner)
+        client.subscribe(topic_connect)
+
+    def on_message_callback(client: mqtt.Client, userdata,
+                            message: MQTTMessage):
+
         payload_dict = json.loads(message.payload)
-        # logger.debug(payload_dict)
+
         if message.topic == topic_scanner:
+            # Just log for information, as using first_found
             id = payload_dict['id']
             name = payload_dict['name']
             action = payload_dict['action']
             logger_scanner.debug(f"{action}: {name} {id}")
+
         elif message.topic == topic_connect:
             logger_connect.debug(
                 f"{payload_dict['sender']}: {payload_dict['state']}")
+            if payload_dict['event_time'] < run_start:
+                logger.debug("Skipping retained message")
+            else:
+                if payload_dict['sender'] == 'DE1':
+                    event = ready_de1
+                else:  # Right now, it's either a DE1 or a Scale
+                    event = ready_scale
+                if payload_dict['state'] == ConnectivityState.READY.value:
+                    event.set()
+                else:
+                    event.clear()
+
         else:
             pass
 
@@ -149,8 +195,13 @@ async def run():
         transport=MQTT_TRANSPORT,
     )
 
+    if DEBUG_MQTT:
+        mqtt_logger = logging.getLogger('Paho')
+        mqtt_logger.setLevel(logging.DEBUG)
+        mqtt_client.enable_logger(mqtt_logger)
+
     mqtt_client.on_connect = on_connect_callback
-    mqtt_client.on_message = scan_message_cb
+    mqtt_client.on_message = on_message_callback
 
     mqtt_client.connect(host=MQTT_BROKER_HOSTNAME,
                    port=MQTT_BROKER_PORT,
@@ -160,25 +211,23 @@ async def run():
                    clean_start=MQTT_CLEAN_START_FIRST_ONLY,
                    properties=None)
 
-    mqtt_client.subscribe(topic_scanner)
-    mqtt_client.subscribe(topic_connect)
     mqtt_client.loop_start()
 
-    # Wait a second for the loop to start (ugly)
-    await asyncio.sleep(1)
+    check_connectivity(Resource.DE1_CONNECTIVITY, have_de1, ready_de1)
+    check_connectivity(Resource.SCALE_CONNECTIVITY, have_scale, ready_scale)
 
     t0 = time.time()
 
-    find_first(Resource.DE1_ID)
-    find_first(Resource.SCALE_ID)
+    if not have_de1.is_set():
+        find_first(Resource.DE1_ID)
+    if not have_scale.is_set():
+        find_first(Resource.SCALE_ID)
 
     t1 = time.time()
     logger.debug(f"##### Connection time: {(t1 - t0):.3f} seconds")
 
-    # TODO: How to manage "ready" event if the DE1 or scale
-    #       was already connected and ready when this started?
+    ready_de1.wait()
 
-    # TODO: This should wait for DE1 ready
     upload_profile('/home/ble-remote/devel/pyDE1/examples/jmk_eb6.json')
     set_saw(50.0)
 
@@ -191,7 +240,6 @@ async def run():
 
     logger.info("Done")
 
+
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.set_debug(True)
-    loop.run_until_complete(run())
+    run()
